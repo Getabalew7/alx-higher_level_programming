a3b1c2ca28067aa815a2c0d95f8e3d2b082eeaef
Getabalew A <60742895+Getabalew7@users.noreply.github.com>
2022-10-04 20:15:01 +0300

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,4 +1,13 @@
 #!/usr/bin/node
-const list = require('./100-data.js');
-console.log(list.list);
-console.log(list.list.map((element, idx) => element * idx));
+// Script imports a dictionary of occurrences by user id
+// and computes a dictionary of user ids by occurrence.
+
+const { dict } = require('./101-data.js');
+const Dictn = {};
+for (const N in dict) {
+    if (Dictn[dict[N]] === undefined) {
+	Dictn[dict[N]] = [];
+    }
+    Dictn[dict[N]].push(N);
+}
+console.log(Dictn);
